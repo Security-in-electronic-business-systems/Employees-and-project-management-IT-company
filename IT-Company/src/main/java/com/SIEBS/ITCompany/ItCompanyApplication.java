631ff4eac3b540f86520f222b9e8e0050af74228
Dejan Gloginjic <deki555@hotmail.com>
2023-05-15 15:48:28 +0200

    initial commit

---
+++ IT-Company/src/main/java/com/SIEBS/ITCompany/ItCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.SIEBS.ITCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItCompanyApplication.class, args);
+	}
+
+}
