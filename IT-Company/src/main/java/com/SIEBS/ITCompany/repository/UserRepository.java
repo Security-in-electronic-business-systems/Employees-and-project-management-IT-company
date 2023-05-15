462fd3ea1336ba2a39e859970812d17f006945cc
Dejan Gloginjic <deki555@hotmail.com>
2023-05-15 21:25:28 +0200

    pomocna

--- IT-Company/src/main/java/com/SIEBS/ITCompany/repository/UserRepository.java
+++ IT-Company/src/main/java/com/SIEBS/ITCompany/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.SIEBS.ITCompany.repository;
 
 import com.SIEBS.ITCompany.model.User;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
 @Repository
-public interface UserPostgresRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  Optional<User> findByEmail(String email);
+
 }
