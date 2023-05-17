6977fedb800e2059189c20373d782bafb431f2d8
Tamara Antic <tasaantic00@gmail.com>
2023-05-17 23:29:18 +0200

    allowed test email for unauthorized users

--- IT-Company/src/main/java/com/SIEBS/ITCompany/config/SecurityConfiguration.java
+++ IT-Company/src/main/java/com/SIEBS/ITCompany/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
         .csrf()
         .disable()
         .authorizeHttpRequests()
-        .requestMatchers("/api/v1/auth/**")
+        .requestMatchers("/api/v1/auth/**", "/api/v1/user/testEmail")
           .permitAll()
         .anyRequest()
           .authenticated()
--- IT-Company/src/main/java/com/SIEBS/ITCompany/controller/UserController.java
+++ IT-Company/src/main/java/com/SIEBS/ITCompany/controller/UserController.java
@@ -3,6 +3,7 @@ package com.SIEBS.ITCompany.controller;
 
 import com.SIEBS.ITCompany.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
-
+    @Autowired
     private final EmailService emailService;
 
     @GetMapping("/testEmail")
