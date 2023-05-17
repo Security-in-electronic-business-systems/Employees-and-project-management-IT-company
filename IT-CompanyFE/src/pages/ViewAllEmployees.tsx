51a9da4fb2c306749138d7eb973408e66e78f62a
Danijela Kralus <kralusdanijela@gmail.com>
2023-05-17 23:45:33 +0200

    implement view all

--- IT-CompanyFE/src/pages/ViewAllEmployees.tsx
+++ IT-CompanyFE/src/pages/ViewAllEmployees.tsx
@@ -1,22 +1,69 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
+interface User {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phoneNumber: string;
+  isApproved: boolean;
+  title: string;
+  address: Address;
+}
+
+interface Address {
+  street: string;
+  number: string;
+  city: string;
+  country: string;
+}
 
 function ViewAllEmployees() {
-  const [message, setMessage] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-     fetch('https://localhost:8081/api/v1/auth/message')
-     .then(response => response.json())
-     .then(data => {
-        setMessage(data);
-     })
-     .catch(error => console.log(error));
+    fetch('https://localhost:8081/api/v1/auth/message')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return (
     <div>
-      <h1>Fetch Example</h1>
-      {message ? <p>{message}</p> : <p>Loading...</p>}
+      <h1>Prikaz korisnika</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Ime</th>
+            <th>Prezime</th>
+            <th>Email</th>
+            <th>Telefon</th>
+            <th>Odobren</th>
+            <th>Naslov</th>
+            <th>Adresa</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.firstname}</td>
+              <td>{user.lastname}</td>
+              <td>{user.email}</td>
+              <td>{user.phoneNumber}</td>
+              <td>{user.isApproved ? 'Da' : 'Ne'}</td>
+              <td>{user.title}</td>
+              <td>
+                {user.address &&
+                  `${user.address.street} ${user.address.number}, ${user.address.city}, ${user.address.country}`}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
