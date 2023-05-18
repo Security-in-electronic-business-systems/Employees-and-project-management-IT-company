8e1e909c7541345be777d987153e1c6f38984976
Dejan Gloginjic <deki555@hotmail.com>
2023-05-18 17:07:01 +0200

    bug fix

--- IT-CompanyFE/src/App.tsx
+++ IT-CompanyFE/src/App.tsx
@@ -7,7 +7,6 @@ import { Login } from "./pages/Login";
 import { PasswordlessLogin } from "./pages/PasswordlessLogin";
 import ViewAllEmployees from './pages/ViewAllEmployees';
 import WaitRoom from './pages/WaitRoom';
-import Protected from './authorization/Protected';
 
 function App(){
   useEffect(() => {
@@ -21,7 +20,7 @@ function App(){
         <Routes>
           <Route path="/ViewAll" element={<ViewAllEmployees/>} />
           <Route path="/register" element={<Register/>} />
-          <Route path="/" element={<Protected role={"PROJECT_MANAGE"}><Home/></Protected>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/passwordless-login" element={<PasswordlessLogin/>} />
           <Route path="/wait-room" element={<WaitRoom/>} />
